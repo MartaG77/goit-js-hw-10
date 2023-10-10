1ded141a7a309ba7ef81e9abe549580cc12f5262
MartaG77 <129276573+MartaG77@users.noreply.github.com>
2023-10-10 16:36:29 +0200

    update

---
+++ src/JS/cat-api.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = "live_gg2omrUzVpAVvRPqOLFmN1ZAzv92LAJM023W1kdHVr38NFEGa93pZqCzRbKsfEvL"
+
+fetchBreeds()
+fetchCatByBreed(breedId)
